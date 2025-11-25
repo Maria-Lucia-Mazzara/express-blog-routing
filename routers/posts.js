db2176b54d45e8de2ed07d3551fe10498d55bb04
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-25 13:08:42 +0100

    aggiunto nel documento server.js gli express static e fatto diventare h1 il titolo della respons. nael documento posts fatta la logica per selezionare id

--- routers/posts.js
+++ routers/posts.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const app = express()
+const router = express.Router()
 const array = require('../dati/posts_array')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+router.get('/', (req, res) => {
+    res.send(array);
+});
+
+router.get('/:id', (req, res) => {
+    res.send(`cibo id: ${req.params.id}`);
 })
 
+router.post('/', (req, res) => {
+    res.send('mostro nuova array cibo')
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express()
 const port = 3000
 const posts = require('./routers/posts')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>Lista di cibo vario</h1>')
 })
